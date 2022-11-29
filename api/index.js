6f0cf40b39168865699676dcb9b5cac65d6f483c
Zeenat <zeenatavontuur@gmail.com>
2022-11-29 15:31:45 +0200

    commit

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const app = express();
+const pgp = require('pg-promise')({})
+const jifunze_ffs = require('./factory-function');
+
+let useSSL = false;
+let local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local) {
+    useSSL = true;
+}
+
+const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://postgres:pg123@localhost:5432/shoes_api';
+
+const config = {
+    connectionString: DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const db = pgp(config);
+const jifunze = jifunze_ffs(db);
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.use(cors())
