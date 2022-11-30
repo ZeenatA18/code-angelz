9f3390243e4ccecc8252263ee6c5e5230d18f481
Nonkululeko Nooi <nooi9423@gmail.com>
2022-11-30 23:01:21 +0200

    Nkuli

---
+++ nkuli/factory-function.js
@@ -0,0 +1,6 @@
+module.exports = function Waiter(db) {
+
+    return {
+       
+    }
+}
---
+++ nkuli/public/index.js
@@ -0,0 +1,10 @@
+let add_score = 0;
+
+
+let scoreElement = document.querySelector('.add_score');
+// console.log(scoreElement)
+scoreElement.innerText = add_score;
+// console.log(unt)
+
+const score_box = document.querySelector('.score_box');
+
